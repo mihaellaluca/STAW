0e91ec570e69337385f56262c1049d06d53d4b07
AmarandeiGeorge <gamarandei20@yahoo.com>
2020-02-09 18:57:59 +0200

    Update map-component.js

--- frontend/map/map-component.js
+++ frontend/map/map-component.js
@@ -1,66 +1,70 @@
 class MapComponent extends HTMLElement {
-  getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
-  addMapScript(){
-    var scriptNode;
-    var scriptText;
-    console.log('adding Map Scripts');
-    fetch('http://localhost:3000/products/coords', {
-			method: 'POST',
-			headers: new Headers({'auth':`${this.getCookie("token")}`})
-	  	})
-    .then((res)=>{return res.json()})
-    .then((data)=>{
-      let result = `    function initMap() {
+	getCookie(cname) {
+		var name = cname + '=';
+		var decodedCookie = decodeURIComponent(document.cookie);
+		var ca = decodedCookie.split(';');
+		for (var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') {
+				c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length, c.length);
+			}
+		}
+		return '';
+	}
+	addMapScript() {
+		var scriptNode;
+		var scriptText;
+		console.log('adding Map Scripts');
+		fetch('http://localhost:3000/products/coords', {
+			method: 'GET',
+			headers: new Headers({ auth: `${this.getCookie('token')}` })
+		})
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				let result = `    function initMap() {
         var uluru = {lat: 47.151726, lng: 27.587914};
         var map = new google.maps.Map(
             document.getElementById('map'), {zoom: 8, center: uluru});
         var marker = new google.maps.Marker({position: uluru, map: map});`;
-        data.forEach((coord)=>{
-          const {lat,lng} = coord;
-          result +=`var marker = new google.maps.Marker({position:{${lat},${lng}},map:map});`
-        });
-        result +='}';
-        scriptNode = document.createElement('script');
-        scriptText = document.createTextNode(result);
-        scriptNode.appendChild(scriptText);
-    });
-     console.log(this.getCookie('token'));
-    // var scriptNode = document.createElement('script');
-    // var scriptText = document.createTextNode(`
-    // function initMap() {
-    //   var uluru = {lat: 47.151726, lng: 27.587914};
-    //   var map = new google.maps.Map(
-    //       document.getElementById('map'), {zoom: 8, center: uluru});
-    //   var marker = new google.maps.Marker({position: uluru, map: map});
-    // }`);
-    scriptNode.appendChild(scriptText);
-    var scriptNode2= document.createElement('script');
+				data.forEach((coord) => {
+					const { lat, lng } = coord;
+					result += `var marker = new google.maps.Marker({position:{lat:${lat},lng:${lng}},map:map});`;
+				});
+				result += '}';
+				scriptNode = document.createElement('script');
+				scriptText = document.createTextNode(result);
+				scriptNode.appendChild(scriptText);
+				document.querySelector('body').appendChild(scriptNode);
+			});
+		console.log(this.getCookie('token'));
+		// var scriptNode = document.createElement('script');
+		// var scriptText = document.createTextNode(`
+		// function initMap() {
+		//   var uluru = {lat: 47.151726, lng: 27.587914};
+		//   var map = new google.maps.Map(
+		//       document.getElementById('map'), {zoom: 8, center: uluru});
+		//   var marker = new google.maps.Marker({position: uluru, map: map});
+		// }`);
+		//scriptNode.appendChild(scriptText);
+		var scriptNode2 = document.createElement('script');
 
-    var scriptNode2= document.createElement('script');
+		var scriptNode2 = document.createElement('script');
 
-    scriptNode2.setAttribute('src','https://maps.googleapis.com/maps/api/js?key=AIzaSyBVpNtqt0RlR9f8zKAHeUdYgbJiEHV8jCM&callback=initMap');
+		scriptNode2.setAttribute(
+			'src',
+			'https://maps.googleapis.com/maps/api/js?key=AIzaSyBVpNtqt0RlR9f8zKAHeUdYgbJiEHV8jCM&callback=initMap'
+		);
 
-    document.querySelector("body").appendChild(scriptNode);
-    document.querySelector("body").appendChild(scriptNode2);
+		document.querySelector('body').appendChild(scriptNode2);
+	}
 
-  }
-
-    connectedCallback() {
-      this.innerHTML = `
+	connectedCallback() {
+		this.innerHTML = `
       <link rel="stylesheet" href="map/map-style.css">
     <header-component></header-component>
     <style>
@@ -75,9 +79,7 @@ class MapComponent extends HTMLElement {
     
 
 `;
-  
-    }
-  }
-  
-  customElements.define("map-component", MapComponent);
-  
+	}
+}
+
+customElements.define('map-component', MapComponent);
