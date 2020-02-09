a40ee0f7524ec05cd737724d15cbae9fc68683a9
mihaellaluca <48389197+mihaellaluca@users.noreply.github.com>
2020-02-09 22:30:25 +0200

    added shopping-cart

--- frontend/shopping-cart/shopping-cart-component.js
+++ frontend/shopping-cart/shopping-cart-component.js
@@ -1,39 +1,49 @@
-class ShoppingCartComponent extends HTMLElement {
+import { CartProductComponent } from "./../shopping-cart/cart-products.js";
 
-    connectedCallback() {
+export class ShoppingCartComponent extends HTMLElement {
+  static cartProducts = [];
+  priceSum = 0;
 
-      this.innerHTML = `
+  render() {
+    let shoppingList = localStorage.getItem('shopping-list');
+    // console.log(typeof(shoppingList));
+    if (ShoppingCartComponent.cartProducts.length === 0) {
+      const cartProduct = document.querySelector("#my-cart");
+      cartProduct.innerHTML = "No product added";
+      cartProduct.style.backgroundColor = "transparent";
+      document.getElementById("shoppingPrice").style.display = "none";
+    } else {
+      ShoppingCartComponent.cartProducts.forEach(product => {
+        const cartProduct = document.querySelector("#my-cart");
+        const comp = new CartProductComponent(product);
+        comp.content = product;
+        cartProduct.appendChild(comp);
+      });
+    }
+  }
+
+  connectedCallback() {
+    ShoppingCartComponent.cartProducts.forEach(product => {
+      let productPrice = product.price.match(/\d/g).join("");
+      this.priceSum += parseFloat(productPrice);
+    });
+
+    this.innerHTML = `
             <link rel="stylesheet" href="shopping-cart/shopping-cart-style.css">
             <header-component></header-component>
             <h1>My Shopping Cart</h1>
-            <div id="shoppingListAndPrice">
-                <div id="shoppingList">
-                    <div id="product">
-                        <p id="title">Baterie reincarcabila Lipo</p>
-                        <p id="description">Incarcator pentru baterii drone Hubsan, Syma, Wltoys, cu 5 sloturi</p>
-                        <p id="price">Price: 20$</p>
-                        <p id="pieces">Pieces: 1</p>
-                        <button id="remove">Remove</button>
-                    </div>
-                    <div id="product">
-                    <p id="title">Baterie reincarcabila Lipo</p>
-                    <p id="description">Incarcator pentru baterii drone Hubsan, Syma, Wltoys, cu 5 sloturi</p>
-                    <p id="price">Price: 20$</p>
-                    <p id="pieces">Pieces: 2</p>
-                    <button id="remove">Remove</button>
-                </div>
-                </div>
-                <div id="shoppingPrice">
-                <p id="total">TOTAL: 45$</p>
-                <p id="price">Price: 40$</p>
-                <p id="transport">Transport: 5$ </p>
-                <button id="placeOrder">Place Order</button>
-                </div>
+            <div class="cart-products" id="my-cart"></div>
+            <br>
+            <div id="shoppingPrice">
+              <p id="total">TOTAL: ${this.priceSum+5}$</p>
+              <p id="price">Products: ${this.priceSum}$</p>
+              <p id="transport">Transport: 5$ </p>
+              <button id="placeOrder">Place Order</button>
             </div>
-
           `;
-    }
+
+    this.render();
   }
-  
-  customElements.define("shopping-cart-component", ShoppingCartComponent);
-  
+}
+
+customElements.define("shopping-cart-component", ShoppingCartComponent);
